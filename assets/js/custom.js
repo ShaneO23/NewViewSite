2d87d8a49f13153a0feb7f84b850fd7731da9f08
ShaneO23 <shane@Shanes-MacBook-Pro.local>
2016-02-18 17:12:49 +0100

    custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,8 +39,8 @@
             $(function () {
                 $.vegas('slideshow', {
                     backgrounds: [
-                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
-                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
+                      { src: 'assets/img/background1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
+                      { src: 'assets/img/background2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
                        //{ src: 'assets/img/3.jpg', fade: 1000, delay: 9000 }, 
                      
                     ]
